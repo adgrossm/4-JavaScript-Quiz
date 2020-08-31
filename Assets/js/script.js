37d8c7c6069582500ce333df8f82166bc8c9ced8
Adam Grossman <adgrossm@gmail.com>
2020-08-31 09:57:21 -0400

    update file structure and README

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,17 @@
+var startScreen = document.getElementById("startScreen")
 var startButton = document.getElementById("start")
 var questionScreen = document.getElementById("gameField")
-var secondsLeft =75
+var questionEl = document.getElementById("question")
 var answersEl = document.getElementById("answers")
+var timerEl = document.getElementById("timer")
+var linkHighscores = document.getElementById("linkHighscores")
+var submitInitialsForm = document.getElementById("submitInitialsForm")
+var initialsInp = document.getElementById("initials")
+var addScoreEl = document.getElementById("addScore")
+var secondsLeft = 75
 var timer
 var currentQuestion = 0
+
 var questions = [
     {
         question: "What is NOT a JavaScript data type?",
@@ -30,57 +38,74 @@ var questions = [
         answers: ["JavaScript", "Joe Schmoe", "Just Syntax", "Jolly Spider"],
         correct: 0
     }
-   
+
 ];
 
-function time(){
-    timer = setInterval(function(){
-        secondsLeft--
-        document.getElementById("timer").textContent = "Time: " + secondsLeft + " sec" 
-        if(secondsLeft === 0){
-            clearInterval(timer)
-            console.log('end game')
-        }
+startButton.addEventListener("click", init)
+submitInitialsForm.addEventListener("submit", updateScores)
 
-    },1000)
-}
 
-function showQuestion (){
-    var questionEl = document.getElementById("question")
-    answersEl.innerHTML =""
-    questionEl.textContent = questions[currentQuestion].question
-    for (let i = 0; i < questions[currentQuestion].answers.length; i++) {
-        var button = document.createElement("button")
-        button.textContent = questions[currentQuestion].answers[i]
-        button.setAttribute('value', i )
-        button.classList.add("answer","btn", "btn-primary")
-        answersEl.appendChild(button)
-    }
-
-}
-
-function init(){
+function init() {
     //start timer
     time()
     //make welcom screen invisible
-    var startScreen = document.getElementById("startScreen")
     startScreen.setAttribute("class", "d-none")
     //display first question
     questionScreen.classList.remove("d-none")
     showQuestion()
 }
 
-function checkAnswer(){
-    if (!event.target.classList.contains('answer')) return;
-    var userGuess = parseInt(event.target.value)
-if(userGuess !== questions[currentQuestion].correct){
-        secondsLeft = secondsLeft - 15
-        console.log('you are wrong')
-    }
-    currentQuestion++
-    showQuestion()
-
+function time() {
+    timer = setInterval(function () {
+        secondsLeft--
+        timerEl.textContent = "Time: " + secondsLeft + " sec"
+        if (secondsLeft <= 0) {
+            endGame()
+        }
+    }, 1000)
 }
 
-answersEl.addEventListener("click", checkAnswer)
-startButton.addEventListener("click", init)
+function showQuestion() {
+    answersEl.innerHTML = ""
+    questionEl.textContent = questions[currentQuestion].question
+    for (var i = 0; i < questions[currentQuestion].answers.length; i++) {
+        var button = document.createElement("button")
+        button.textContent = questions[currentQuestion].answers[i]
+        button.setAttribute('value', i)
+        button.classList.add("answer", "btn", "btn-primary")
+        button.addEventListener("click", checkAnswer)
+
+        answersEl.appendChild(button)
+    }
+}
+
+function checkAnswer() {
+    var userGuess = parseInt(this.value)
+    if (userGuess !== questions[currentQuestion].correct) {
+        secondsLeft -= 15
+        console.log('you are wrong')
+    }
+
+    currentQuestion++
+
+    if (currentQuestion < questions.length) {
+        showQuestion()
+    } else {
+        endGame()
+    }
+}
+
+function endGame() {
+    clearInterval(timer)
+    console.log('end game')
+
+    questionScreen.classList.add("d-none")
+    addScore.classList.remove("d-none")
+}
+
+function updateScores(event){
+    event.preventDefault();
+
+    var initials = initialsInp.value
+    console.log(initials, secondsLeft)
+}
